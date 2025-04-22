20f7b2d148b0b10fedd379a835ade57cbaad1dbb
picascaz <lzymelo@proton.me>
2025-04-22 11:06:42 +0800

    [beta][staging] update ui

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -58,17 +58,18 @@ export function PageLayout({ children }: { children: ReactNode }) {
           // @ts-expect-error
           { '--width': '15rem' }
         }
+
         toastOptions={{
           classNames: {
             toast: "rounded-xl bg-[#585858] border border-solid border-white/10 text-white/60 text-xs px-4 py-2",
-            error: '!bg-[#FFD4D7] flex !items-start text-xs font-medium ',
+            error: '!bg-[#FFD4D7] flex !items-start  text-xs  gap-5 ',
             warning: '!bg-[#585858] !border-[#585858] !text-[#FFF] !gap-5 ',
-            title: '!leading-[1.2]'
+            title: '!leading-[1.2] !text-sm !font-normal',
           }
         }}
         icons={{
-          error: <IoIosCloseCircle className="text-[#FF3A3D] text-sm" />,
-          warning: <CiWarning className="text-[#FFF] text-sm " />
+          error: <IoIosCloseCircle className="text-[#FF3A3D] text-2xl pt-2" />,
+          warning: <CiWarning className="text-[#FFF] text-sm   " />
         }}
       />
       <Providers>{children}</Providers>
