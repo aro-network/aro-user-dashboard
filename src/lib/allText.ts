0966a297a6c81c8197e5886a3a58a779255ca83b
picascaz <lzymelo@proton.me>
2025-06-04 11:12:36 +0800

    all text

---
+++ src/lib/allText.ts
@@ -0,0 +1,187 @@
+export const AllText = {
+  tab: {
+    "Edge Nodes": "Edge Nodes",
+    Stats: "Stats",
+    Funds: "Funds",
+    Referral: "Referral",
+    "ARO ID": "ARO ID",
+  },
+  signIn: {
+    title: "Sign In For Devnet",
+    titleTips:
+      " Devnet is for closed test only. Devnet is not Testnet. No mining rewards will be generated in Devnet. Testnet is coming soon. To join Devnet, please refer to the Pioneer Program.",
+    noAccount: "Don’t have an account?",
+    signUp: "Sign Up",
+    forgetPwd: "Forget Password?",
+  },
+  signUp: {
+    title: "Sign Up For Devnet",
+    titleTips:
+      " Devnet is for closed test only. Devnet is not Testnet. No mining rewards will be generated in Devnet. Testnet is coming soon. To join Devnet, please refer to the Pioneer Program.",
+    agreeText: "I agree to the ARO  Term of Service and Privacy Policy",
+    haveAccount: "Already have an account? Sign In",
+  },
+  resetPwd: {
+    title: "Reset Password",
+    goBack: "Go back",
+  },
+  edgeNodes: {
+    "All Nodes": "All Nodes",
+    noData:
+      "ARO supports hardware and software solutions on multiple platforms for running an Edge Node. Find which node type is best for you in this guide.",
+    "Add Your Edge Node": "Add Your Edge Node",
+  },
+  AAddNewNodes: {
+    title: "Please choose which type of Edge Node you want to add:",
+    Hardware: "Hardware",
+    Software: "Software",
+    "See Guidance": "See Guidance",
+    type: {
+      step1: {
+        title: "Step 1: Connect your device",
+        subtitle:
+          "Make sure your xx is powered on and connected to the internet (with internet cable). Find the serial number (19-digit numbers) on your xxx and fill in:",
+        error: [
+          "Sorry, we cannot find your xx. Please make sure your xx is powered on and have internet access.",
+        ],
+      },
+      step2: {
+        title: "Step 2: Add Device to your account",
+        "Device found": "Device found:",
+        "Device Type": "Device Type",
+        "Serial Number": "Serial Number",
+        "Network Status": "Network Status",
+        "Device IP": "Device IP",
+        error: [
+          "This device has been already added to an ARO Account. Please delete device to create a new add.",
+          "Please make sure your device is still online. Otherwise, the add process will fail.",
+        ],
+      },
+      step3: {
+        "Step 3: Configure your Edge Node": "Step 3: Configure your Edge Node",
+        "Set a name for your Edge Node": "Set a name for your Edge Node",
+        "You can change the name anytime later.":
+          "You can change the name anytime later.",
+      },
+      step4: {
+        "Congratulations!": "Congratulations!",
+        content: "Edge Node (Device Type: xxx) add successful.",
+      },
+    },
+  },
+  delDevice: {
+    "Device found": "Device found:",
+    "Device Type": "Device Type",
+    "Serial Number": "Serial Number",
+    "Network Status": "Network Status",
+    "Device IP": "Device IP",
+    "Confirm Delete": "Confirm Delete",
+    "Please make sure you want to delete this device before continue. You cannot undo this action.":
+      "Please make sure you want to delete this device before continue. You cannot undo this action.",
+    "Are you sure you really want to delete Edge Node?":
+      "Are you sure you really want to delete Edge Node?",
+    "Congratulations!": "Congratulations!",
+    delInfo: "Edge Node (Device Type: xxx delete successful.",
+  },
+  stats: {
+    "My Nodes": {
+      "My Nodes": "My Nodes",
+      "Total Nodes": "Total Nodes",
+      Online: "Online",
+    },
+    "Rewards - All Nodes": {
+      "Rewards - All Nodes": "Rewards - All Nodes",
+      Total: "Total",
+      Today: "Today",
+    },
+    Trending: "Trending",
+    rewardsAllNodesTips:
+      "The rewards number shows how much you have earned from running Edge Nodes, plus the Referral Bonus.",
+    inviteTips:
+      "You enjoy a 20% extra boost on node rewards for the first 14 days from being referred to ARO. ",
+  },
+  Funds: {
+    Funds: "Funds",
+    "Rewards & Claim": "Rewards & Claim",
+    "Total Claimable": "Total Claimable",
+    "Claim to Address": "Claim to Address",
+    "Total Claimed": "Total Claimed",
+    History: "History",
+    Date: "Date",
+    Time: "Time",
+    "Transaction Type": "Transaction Type",
+    Amount: "Amount",
+    Address: "Address",
+    TXID: "TXID",
+    "Rewards claim not applicable on current stage.":
+      "Rewards claim not applicable on current stage.",
+  },
+
+  referral: {
+    "My Referral Code": "My Referral Code",
+    "Copy Referral Link": "Copy Referral Link",
+    "Referral Bonus": "Referral Bonus",
+    referralBonusTips: `As a referrer, you earn a commission from your referee's node rewards.`,
+    whitelistedTips:
+      "You get whitelisted for the extra second-tier commission.",
+    isInvitedTips:
+      "Your first-tier referee count and second-tier referee count.",
+    "How Referral Program Works?": "How Referral Program Works?",
+    subTitle: `The Referrer enjoy 15% commission from the Referee's mining rewards.
+Contact us to get whitelisted for the extra seond-tier commission.`,
+  },
+  aroId: {
+    evmAddressTips: "This function is not available in Devnet.",
+    notReferredTips: "You are not referred by anyone.",
+    addReferrer: {
+      title: "Add Referrer",
+      content: `You can add a Referrer (the one that invited you to ARO) by filling the Referral Code. Being referred an ARO user will give you 20% extra boost on your Edge Node rewards for 14 days. Be careful: you cannot change your referrer information once it is set.`,
+    },
+  },
+  signOut: {
+    tips: `You are going to log out your account.Are you sure?`,
+  },
+  devnet: {
+    title:
+      "Devnet is for closed test only. Devnet is not Testnet. No mining rewards will be generated in Devnet. Testnet is coming soon. To join Devnet, please refer to the xxx",
+  },
+  nodeDetails: {
+    "Node Info": {
+      "Node Name": "Node Name",
+      "Node ID": "Node ID",
+      "Node Type": "Node Type",
+    },
+    Rewards: {
+      Total: "Total",
+      Today: "Today",
+      Yesterday: "Yesterday",
+    },
+    "Rewards History": "Rewards History",
+    Basics: {
+      Basics: "Basics",
+      "Create Date": "Create Date",
+      "Serial Number": "Serial Number",
+      Device: "Device",
+      "Registered Region": "Registered Region",
+      "Reputation Point": "Reputation Point",
+      "Cheat Status": "Cheat Status",
+    },
+    "Network Info": {
+      "Network Info": "Network Info",
+      "Public IP": "Public IP",
+      "IP Location": "IP Location",
+      "Local IP": "Local IP",
+      "MAC Address": "MAC Address",
+    },
+    "Device States": {
+      "CPU Cores": "CPU Cores",
+      "CPU Use": "CPU Use",
+      RAM: "RAM",
+      ROM: "ROM",
+    },
+  },
+  right: {
+    " My ARO ID": " My ARO ID",
+    "Sign Out Accout": "Sign Out Accout",
+  },
+};
