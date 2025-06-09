917f8449fce5bf91f9c581eccaa3748cd19f7785
picascaz <158241034+picascaz@users.noreply.github.com>
2025-06-10 01:16:45 +0800

    [all] Update AUnbind.tsx

--- src/components/EdgeNode/components/AUnbind.tsx
+++ src/components/EdgeNode/components/AUnbind.tsx
@@ -7,7 +7,6 @@ import { useQuery } from "@tanstack/react-query"
 import { FC, ReactNode, useEffect, useState } from "react"
 import { foundDeviceList } from "./AAddNewNodes"
 import useMobileDetect from "@/hooks/useMobileDetect"
-import { getItem, removeItem } from "@/lib/storage"
 import { useSearchParams } from "next/navigation"
 import { AllText } from "@/lib/allText"
 
@@ -20,25 +19,19 @@ const AUnbind: FC<{ nodeId: string, onBack: () => void }> = ({ nodeId, onBack })
   const [isConfirm, setIsConfirm] = useState(false)
   const isMobile = useMobileDetect()
   const searchParams = useSearchParams();
+  const [isFetching,setIsFetching] = useState(true)
+  const [delDetail,setDelDetail]= useState<Nodes.DevicesInfo>()
 
   const params = new URLSearchParams(searchParams.toString());
   const nId = params.get("nId") || ''
 
-  const { data, refetch, isFetching } = useQuery({
-    queryKey: ["DeviceUnbindStatus"],
-    enabled: false,
-    queryFn: async () => {
-      const res = await backendApi.getDeviceStatusInfo(nId)
-      return res
 
-    },
-    staleTime: 0,  
-    gcTime: 0,     
-    refetchOnMount: "always",
-    refetchOnWindowFocus: false,
+  const currentStatus = async() =>{
+    const res = await backendApi.getDeviceStatusInfo(nId)
+    setDelDetail(res)
+    setIsFetching(false)
 
-  });
-
+  }
 
   const getStatus = useQuery({
     queryKey: ["NodeStatuList"],
@@ -46,22 +39,21 @@ const AUnbind: FC<{ nodeId: string, onBack: () => void }> = ({ nodeId, onBack })
     queryFn: () => backendApi.unbingDevice(nId)
   });
 
-  const isOwner = useQuery({
-    queryKey: ["unBindOwner", nId],
-    enabled: true,
-    queryFn: () => backendApi.currentOwner(nId),
-  
-  });
+
+  const isUserOwner = async() =>{
+    const isOwner = await backendApi.currentOwner(nId)
+    
+    if (isOwner?.owner === false) {
+      onBack(); 
+    }else{
+      currentStatus()
+    }
+  }
 
 
   useEffect(() => {
-
-    if (isOwner.data?.owner === false) {
-      onBack()
-    } else {
-      refetch()
-    }
-  }, [isOwner.data?.owner])
+    isUserOwner()
+  }, [])
 
   const onUnbindingConfig = async () => {
     setIsConfirm(!isConfirm)
@@ -89,11 +81,9 @@ const AUnbind: FC<{ nodeId: string, onBack: () => void }> = ({ nodeId, onBack })
           <div className="w-[37.5rem] smd:w-full">
             <div className=" py-5 my-5 pl-5 smd:pr-5 bg-[#6D6D6D66] smd:flex-col  w-full flex gap-4 smd:gap-5 rounded-[1.25rem]">
               <div className="w-[45%] smd:w-full smd:h-[12.5rem]">
-                {!data?.nodeType ? <Skeleton className="rounded-2xl"><div className="w-[5.4375rem] rounded-3xl" /></Skeleton> :
-                  <Image src={`./${data?.nodeType}.png`} width={'100%'} height={'100%'} className=" object-contain rounded-lg bg-[#F6F8F9]  w-full h-full" alt={`${data?.nodeType}`} />
-                }
+                  <Image src={`./${delDetail?.nodeType}.png`} width={'100%'} height={'100%'} className=" object-contain rounded-lg bg-[#F6F8F9]  w-full h-full" alt={`${delDetail?.nodeType}`} />
               </div>
-              {foundDeviceList(data, isMobile)}
+              {foundDeviceList(delDetail, isMobile)}
             </div>
             <div className="text-[#FFFFFF80] text-sm mt-[.625rem] smd:mt-5  text-center">
               {AllText.deviceInfo["Please confirm you want to delete this device. This action cannot be undone."]}
@@ -114,7 +104,7 @@ const AUnbind: FC<{ nodeId: string, onBack: () => void }> = ({ nodeId, onBack })
               Congratulations!
             </div>
             <div className="text-center text-sm ">
-              Your Edge Node (Device Type:  <span className=" capitalize">{covertText(data?.nodeType as "box" | "x86" | "Box")})</span> was deleted successfully.
+              Your Edge Node (Device Type:  <span className=" capitalize">{covertText(delDetail?.nodeType as "box" | "x86" | "Box")})</span> was deleted successfully.
             </div>
 
             <div className="flex justify-center items-center flex-col  gap-[.625rem] ">
