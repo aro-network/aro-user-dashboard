a3f5836f1f37a939d843c1868635717f234430eb
picascaz <lzymelo@proton.me>
2025-07-04 17:48:54 +0800

    [staging] fix bug

--- src/components/layouts.tsx
+++ src/components/layouts.tsx
@@ -5,7 +5,7 @@ export function PageUnlogin(p: PropsWithChildren & { childrenClassName?: string,
     return <div className="w-full h-full flex smd:flex-col  ">
         <div className={cn("flex-[4] smd:flex-[1] smd:bg-none smd:mt-[120px]  bg-[url(/mainBg.png)] bg-no-repeat bg-cover bg-center  md:min-h-full  smd:h-full basis-0 flex flex-col justify-center items-center md:overflow-auto smd:gap-5 gap-16 lg:gap-32 text-base", p.headerClassNmae)}>
             {/* {!isMobile && <img src='./mainBg.png' className="w-full h-full" />} */}
-            <img src="https://aro.network/aro-logo.svg" alt="Logo" />
+            <img src="https://aro.network/aro-logo.svg" alt="Logo" className="w-[284px] h-9" />
             <div className="text-[42px] smd:text-lg  leading-normal text-center">Don’t Just Pay for the Internet.<br /> Get Paid for it.</div>
 
         </div>
